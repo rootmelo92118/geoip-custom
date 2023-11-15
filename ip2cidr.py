5e4d8a5903076e9c2a42fac37a49a620c37134c7
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2023-11-15 18:26:06 +0800

    Update ip2cidr.py

--- ip2cidr.py
+++ ip2cidr.py
@@ -2,7 +2,7 @@ from sys import stdin, stdout
 
 def main():
     for line in stdin:
-        stdout.write(str((lambda ip : ip+"\n" if "/" in ip else ip+"/32\n" if "." in ip else ip+"/128\n" if ":" in ip else ip+"\n") (line.strip("\n"))))
+        stdout.write(str((lambda ip : ip+"\n" if "/" in ip else ip+"/32\n" if "." in ip else ip+"/128\n" if ":" in ip else "") (line.strip("\n"))))
     
 if __name__ == '__main__':
     main()
