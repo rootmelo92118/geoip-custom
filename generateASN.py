fc6ae063cda59c1f4b2b2ef4bde3ae09619b8700
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2023-11-16 13:35:49 +0800

    Update generateASN.py

--- generateASN.py
+++ generateASN.py
@@ -3,6 +3,7 @@ import csv, sys
 def main(csv_path=["GeoLite2-ASN-Blocks-IPv4.csv", "GeoLite2-ASN-Blocks-IPv6.csv"], output="./"):
     asnlist = []
     asndict = {}
+    print("Start to Read data...")
     for csvpath in csv_path:
         with open(csvpath, newline='') as csvfile:
             rows = csv.DictReader(csvfile)
@@ -10,11 +11,11 @@ def main(csv_path=["GeoLite2-ASN-Blocks-IPv4.csv", "GeoLite2-ASN-Blocks-IPv6.csv
                 if row['autonomous_system_number'] not in asnlist:
                     asndict[row['autonomous_system_number']] = [row['network']]
                     asnlist.append(row['autonomous_system_number'])
+                    print("Found a new AS Number : AS" + row['autonomous_system_number'])
                 else:
                     asndict[row['autonomous_system_number']].append(row['network'])
-                print(row['network'] + " belongs to AS" + row['autonomous_system_number'])
             csvfile.close()
-
+    print("Data read finished ! \nGenerate plain text file...")
     for i in asnlist:
         exportData = ""
         for j in asndict[i]:
@@ -22,6 +23,7 @@ def main(csv_path=["GeoLite2-ASN-Blocks-IPv4.csv", "GeoLite2-ASN-Blocks-IPv6.csv
         with open(output+"/as"+i,"w") as f:
             f.write(exportData)
             f.close()
+    print("All jobs are done and successful ! ")
         
 if __name__ == '__main__':
     main([sys.argv[1],sys.argv[2]],sys.argv[3])
